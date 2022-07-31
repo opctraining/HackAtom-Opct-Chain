7199bc6d382bd6bfd038b10f92ed5785f22ef02f
HyoeckJinKim <kherootz@gmail.com>
2022-07-31 12:59:51 +0900

    Bank NFT

--- x/hackatomopctchain/keeper/msg_server_create_challenge.go
+++ x/hackatomopctchain/keeper/msg_server_create_challenge.go
@@ -9,14 +9,19 @@ import (
 
 func (k msgServer) CreateChallenge(goCtx context.Context, msg *types.MsgCreateChallenge) (*types.MsgCreateChallengeResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
-
+	badgeMeta := types.BadgeMeta{
+		Name:     msg.BadgeName,
+		Category: msg.Category,
+		Uri:      msg.BadgeURI,
+	}
+	badgeID := k.SetBadgeMeta(ctx, badgeMeta)
 	var challenge = types.Challenge{
 		Creator:  msg.Creator,
 		Category: msg.Category,
 		Date:     msg.Date,
 		Uri:      msg.Uri,
+		RewardId: badgeID,
 	}
 	id := k.StoreChallenge(ctx, challenge)
-
 	return &types.MsgCreateChallengeResponse{Id: id}, nil
 }
